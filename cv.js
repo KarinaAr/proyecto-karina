526351b9f121cd714044205c51fc92ae194ef9d8
KarinaAr <126601105+KarinaAr@users.noreply.github.com>
2023-02-28 16:18:29 -0300

    agregué archivos

---
+++ cv.js
@@ -0,0 +1,14 @@
+
+
+  function boton() {
+    document.getElementById("demo").innerHTML = "33";
+  }
+ 
+  function boton1() {
+    document.getElementById("demo1").innerHTML = "Hotelería";
+  }
+  
+  function boton2() {
+    document.getElementById("demo2").innerHTML = "3";
+  }
+  
